ece99cfd4c675840d31df10d91fa0554076172e2
Varun Gohil <varuncgohil@gmail.com>
2025-04-10 20:10:06 -0400

    removed logic

--- scripts/busy_loop.py
+++ scripts/busy_loop.py
@@ -13,34 +13,10 @@ def main():
                       help='Duration to log in seconds')
     
     args = parser.parse_args()
-    output_file = args.filename
-    cores = [int(core) for core in args.cores.split(',')]
-    
-    # Validate core numbers
-    num_cores = psutil.cpu_count()
-    if any(core >= num_cores for core in cores):
-        print(f"Error: Core numbers must be between 0 and {num_cores-1}")
-        return
 
-    with open(output_file, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        # Write header with core numbers
-        header = ['timestamp'] + [f'cpu_core_{core}' for core in cores]
-        csv_writer.writerow(header)
-
-        start_time = time.time()
-        try:
-            while time.time() - start_time < int(args.duration):
-                timestamp = time.time()
-                # Get utilization for each requested core
-                cpu_info = [get_cpu_utilization(core) for core in cores]
-                csv_info = [timestamp] + cpu_info
-                csv_writer.writerow(csv_info)
-                csvfile.flush()
-                # time.sleep(float(args.sleeptime))
-
-        except KeyboardInterrupt:
-            print("Interrupted. Exiting.")
+    start_time = time.time()
+    while time.time() - start_time < int(args.duration):
+        continue
 
 if __name__ == "__main__":
     main() 
